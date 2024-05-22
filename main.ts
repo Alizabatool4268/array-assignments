140b4567ff9b2feafad336880a011db6026f9f0e
Aliza batool <160774438+Alizabatool4268@users.noreply.github.com>
2024-05-22 06:44:42 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,70 @@
+// QUESTION NO 1
+
+let fruits: string[] = ["apple", "banana", "mango", "orange"];
+console.log(fruits);
+
+// //QUESTION NO 2
+
+let numbers: number[] = [10,20,30,40];
+console.log(numbers);
+
+//QUESTION NO 3
+
+let fruit: string[] = ["apple", "banana", "mango", "orange"];
+let thirdFruit = fruit[2]
+console.log(thirdFruit);
+
+//QUESTION NO 4
+
+let number: number[] = [10,20,30,40];
+number[1]=25;
+console.log(number);
+
+//QUESTION NO 5
+
+let fruitsA: string[] = ["apple", "banana", "mango", "orange"];
+fruitsA.push("grapes");
+console.log(fruitsA);
+
+//QUESTION NO 6
+
+let fruitB = ["apple", "banana", "mango", "orange"];
+let lastFruit = fruitB.pop();
+console.log(fruitB);
+
+//QUESTION NO 7
+
+let fruitC = ["apple", "banana", "mango", "orange"];
+let firstFruit = fruitC.shift();
+console.log(fruitC);
+
+//QUESTION NO 8
+
+let fruitsD: string[] = ["apple", "banana", "mango", "orange"];
+fruitsD.unshift("kiwi");
+console.log(fruitsD);
+
+//QUESTION NO 9
+
+let fruitsF: string[] = ["apple", "banana", "mango", "orange"];
+fruitsF.splice(1,2)
+console.log(fruitsF);
+
+//QUESTION NO 10
+
+let fruitsG: string[] = ["apple", "banana", "mango", "orange"];
+fruitsG.splice(2,2,"pineapple","pear");
+console.log(fruitsG);
+
+//QUESTION NO 11
+
+let fruitH: string[] = ["apple", "banana", "mango", "orange"];
+let citrusFruits: string[] = fruitH.slice(0, 2);
+console.log(citrusFruits);
+
+//QUESTION NO 12
+
+let fruitI: string[] = ["apple", "banana", "mango", "orange"];
+let lastTwoFruits: string[] = fruitI.slice(2, 4);
+console.log(lastTwoFruits);
+
